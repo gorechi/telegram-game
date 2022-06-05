fd829ad9b4c53e167e7c509a55c87caa103d6204
gorechi <gorechi@yandex.ru>
2022-06-06 00:08:55 +0300

    Кнопки быстрых действий теперь опять выстраиваются в два ряда, как это было раньше

--- test.py
+++ test.py
@@ -1,4 +1,9 @@
-a = ' sdfsf sdfs fs sdf sf sf sdf sf sdf sdf'
+a = [1, 2, 3, 4, 5]
 
-print(a[1::])
+def func_chunks_generators(lst, n):
+    for i in range(0, len(lst), n):
+        yield lst[i : i + n]
+        
+new_list = list(func_chunks_generators(a, 2))
 
+print(new_list)
