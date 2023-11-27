532ea79ed821ac0e164d86ecd361dac2901057fd
gorechi <gorechi@yandex.ru>
2023-11-27 22:47:42 +0300

    Начал писать класс торговца

---
+++ class_allies.py
@@ -0,0 +1,8 @@
+from class_items import Book, Key, Money, Rune
+from class_monsters import Monster, Vampire
+from class_protection import Armor, Shield
+from class_room import Furniture, Room
+from class_weapon import Weapon
+from functions import howmany, normal_count, randomitem, showsides, tprint
+from settings import *
+
