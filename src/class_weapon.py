070e935cc8ba1d7af16e7d0a2c94d06577c28672
gorechi <gorechi@yandex.ru>
2025-01-07 08:45:09 +0300

    Исправлена ошибка с отображением оружия и защиты

--- src/class_weapon.py
+++ src/class_weapon.py
@@ -47,7 +47,7 @@ class Weapon:
     
     
     def get_full_names(self, key:str=None) -> str|list:
-        if self.element != 0:
+        if self.element() != 0:
             return self.get_element_names(key)
         if key:
             return self.lexemes.get(key, '')
